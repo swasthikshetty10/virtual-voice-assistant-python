b90a1fc2b932530a2b10b3cd0d0ef6f839f49ec9
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-01-06 20:05:27 +0530

    -__-

--- assistant.py
+++ assistant.py
@@ -1,7 +1,5 @@
 import subprocess 
-import wolframalpha 
 import pyttsx3 
-import tkinter 
 import json 
 import random 
 import operator 
@@ -18,11 +16,6 @@ import ctypes
 import time 
 import requests 
 import shutil 
-from twilio.rest import Client 
-from clint.textui import progress 
-#from ecapture import ecapture as ec
-from bs4 import BeautifulSoup 
-import win32com.client as wincl 
 from urllib.request import urlopen 
 
 engine = pyttsx3.init('sapi5') 
